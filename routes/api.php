e5dab65fe86439a7a99b84bcb75193dd486e57c3
Jose Antonio <jose.lima@fnde.gov.br>
2024-06-08 18:49:04 -0300

    feat: token sanctum funcionando.

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::post('/logs', [LogController::class, 'store']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logs', [LogController::class, 'store']);
 
