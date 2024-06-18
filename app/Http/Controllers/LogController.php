37a32629f811594459bb39fc7b691705a5328619
Jose Antonio <jose.lima@fnde.gov.br>
2024-06-18 13:50:26 -0300

    feat: add route to get data

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Jobs\ProcessLog;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class LogController extends Controller
@@ -37,5 +38,24 @@ class LogController extends Controller
         ProcessLog::dispatch($validated);
 
         return response()->json(['status' => 'Log queued for processing'], 201);
+    }
+
+    public function getLog(Request $request) {
+        $parameters = $request->all();
+        $table = $parameters["table"];
+        $user = $parameters["user"];
+        $date_start = $parameters["date_start"];
+        $date_finish = $parameters["date_finish"];
+
+       
+        if (!isset($table) || !isset($date_start) || !isset($date_finish)) {
+            return response()->json(['error' => 'Missing parameters'], 400);
+        }
+    
+        $results = DB::table($table)
+            ->whereBetween('created_at', [$date_start, $date_finish])
+            ->get();
+    
+        return response()->json($results);
     }
 }
